4b898174c80cd28c54d960512859104425c62953
Pablo Nieto <pablo.nieto@digitaslbi.com>
2017-03-15 18:06:58 +0000

    Cleanup

--- src/js/states/Game.js
+++ src/js/states/Game.js
@@ -1,3 +1,8 @@
+var upKey;
+var downKey;
+var leftKey;
+var rightKey;
+var spaceKey;
 
 BasicGame.Game = function (game) {
 	this.game = game;
@@ -12,52 +17,18 @@ BasicGame.Game = function (game) {
 		'LEFT': 180,
 		'RIGHT': 0
 	};
-	this.DIRECTION_FRAMES = {
-		'up': 9,
-		'down': 0,
-		'left': 3,
-		'right': 6
-	};
-	this.currentDirection = this.DIRECTIONS.UP;
 	this.moving = false;
 };
-
-var upKey;
-var downKey;
-var leftKey;
-var rightKey;
-var spaceKey;
 
 BasicGame.Game.prototype = {
 
 	create: function () {
 		this.game.stage.backgroundColor = '#dddddd';
-
-
-
-		this.blueTank = this.add.sprite(250, 584, 'blueTank');
-		this.blueTank.anchor.setTo(0.5, 0.5);
-		this.blueTank.frame = 6;
-
-
-		this.blueTank.animations.add('moving', [6, 7], 5, true);
-		this.blueTank.animations.add('firing', [6, 8], 4, true);
-
 		this.fireSound = this.add.audio('fireSound');
 
-		upKey = this.input.keyboard.addKey(Phaser.Keyboard.UP);
-		downKey = this.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-		leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-		rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-		spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-		// Game init Code here.
-
-		this.cannon = this.add.weapon(1, 'shot');
-		this.cannon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-		this.cannon.trackSprite(this.blueTank, 8, 0, true);
-		this.cannon.bulletSpeed = 350;
-
+		this.createTank();
+		this.createInput();
+		this.createCannon();
 	},
 
 	update: function () {
@@ -68,42 +39,71 @@ BasicGame.Game.prototype = {
 			this.blueTank.animations.stop();
 		}
 
+		this.handleInput();
+
 		if (spaceKey.justUp) {
-			if (this.cannon.bullets.countLiving() === 0) {
-				this.blueTank.animations.play('firing');
-				this.fireSound.play();
-				this.fire = this.blueTank.addChild(this.make.sprite(8, -4, 'fireExplosion'));
-				this.fire.lifespan = 120;
-			}
-			this.cannon.fire();
+			this.fireCannon();
 		}
 
-		if (leftKey.isDown && this.blueTank.x > 8) {
-			this.currentDirection = this.DIRECTIONS.LEFT;
+	},
+
+	fireCannon: function () {
+		if (this.cannon.bullets.countLiving() === 0) {
+			this.blueTank.animations.play('firing');
+			this.fireSound.play();
+			this.fire = this.blueTank.addChild(this.make.sprite(8, -4, 'fireExplosion'));
+			this.fire.lifespan = 120;
+		}
+		this.cannon.fire();
+	},
+
+	handleInput: function () {
+		if (leftKey.isDown && this.blueTank.x > 16) {
 			this.blueTank.angle = this.DIRECTIONS.LEFT;
 			this.blueTank.x -= this.speed;
 			this.moving = true;
 		}
-		else if (rightKey.isDown && this.blueTank.x < 792) {
-			this.currentDirection = this.DIRECTIONS.RIGHT;
+		else if (rightKey.isDown && this.blueTank.x < 784) {
 			this.blueTank.angle = this.DIRECTIONS.RIGHT;
 			this.blueTank.x += this.speed;
 			this.moving = true;
 		}
-		else if (upKey.isDown && this.blueTank.y > 8) {
-			this.blueTank.y -= this.speed;
+		else if (upKey.isDown && this.blueTank.y > 16) {
 			this.blueTank.angle = this.DIRECTIONS.UP;
-			this.currentDirection = this.DIRECTIONS.UP;
+			this.blueTank.y -= this.speed;
 			this.moving = true;
 		}
-		else if (downKey.isDown && this.blueTank.y < 592) {
-			this.blueTank.y += this.speed;
+		else if (downKey.isDown && this.blueTank.y < 584) {
 			this.blueTank.angle = this.DIRECTIONS.DOWN;
-			this.currentDirection = this.DIRECTIONS.DOWN;
+			this.blueTank.y += this.speed;
 			this.moving = true;
 		} else {
 			this.moving = false;
 		}
+	},
+
+	createTank: function () {
+		this.blueTank = this.add.sprite(16, 584, 'blueTank');
+		this.blueTank.anchor.setTo(0.5, 0.5);
+		this.blueTank.frame = 7;
+		this.blueTank.scale.setTo(2, 2);
+		this.blueTank.animations.add('moving', [7, 6], 10, true);
+		this.blueTank.animations.add('firing', [7, 8], 20, true);
+	},
+
+	createInput: function () {
+		upKey = this.input.keyboard.addKey(Phaser.Keyboard.UP);
+		downKey = this.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+		leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+		rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+		spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	},
+
+	createCannon: function() {
+		this.cannon = this.add.weapon(1, 'shot');
+		this.cannon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+		this.cannon.trackSprite(this.blueTank, 16, 0, true);
+		this.cannon.bulletSpeed = 350;
 	},
 
 	quitGame: function (pointer) {
